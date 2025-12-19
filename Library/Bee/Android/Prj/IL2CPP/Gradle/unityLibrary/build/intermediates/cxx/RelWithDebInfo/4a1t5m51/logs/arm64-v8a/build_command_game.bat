d10c9e78c4aa1be8f0a05eed9de19512df329498
Raphael Carrard <70378163+raphaelcarrard@users.noreply.github.com>
2025-12-18 22:12:48 -0300

    Clean Repository

--- Library/Bee/Android/Prj/IL2CPP/Gradle/unityLibrary/build/intermediates/cxx/RelWithDebInfo/4a1t5m51/logs/arm64-v8a/build_command_game.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"E:\\Unity\\Hub\\Editor\\2023.2.11f1\\Editor\\Data\\PlaybackEngines\\AndroidPlayer\\SDK\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "E:\\My Games\\Cowboy Runner\\.utmp\\RelWithDebInfo\\4a1t5m51\\arm64-v8a" ^
-  game
